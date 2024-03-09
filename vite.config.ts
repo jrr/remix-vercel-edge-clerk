1eddae0816956e42af088fbd53692cf3d4216a06
John Ruble <john.ruble@atomicobject.com>
2024-03-08 21:56:15 -0500

    add polyfills plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vercelPreset } from "@vercel/remix/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [
+    remix({ presets: [vercelPreset()] }),
+    tsconfigPaths(),
+    nodePolyfills({}),
+  ],
 });
